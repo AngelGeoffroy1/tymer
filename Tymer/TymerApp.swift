9a28d430234a6d9bf3df909e513e48b3bc944998
AngelGeoffroy1 <angelgeoffroy@outlook.fr>
2025-12-24 12:53:09 +0100

    Initial Commit

---
+++ Tymer/TymerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TymerApp.swift
+//  Tymer
+//
+//  Created by Angel Geoffroy on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TymerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
